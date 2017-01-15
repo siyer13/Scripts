e758c910f4e5d17fda6472f47415052b3d924c69
Sridhar <sreedharriyer@gmail.com>
2017-01-14 23:18:47 -0800

    Update monitor_job.sh

--- shell/monitor_job.sh
+++ shell/monitor_job.sh
@@ -9,7 +9,7 @@ source /home/siyer/virtualenv/airflow/bin/activate
 function clear_n_run() {
         airflow clear -t $1 -s $execution_date -e $execution_date -c $2
         sleep 5
-       # Running jobs when cleared goes into shutdown state, the next clear, clears that too.
+        # Running jobs when cleared goes into shutdown state, the next clear, clears that too.
         airflow clear -t $1 -s $execution_date -e $execution_date -c $2
         sleep 10
         # start the job

e40298dab9ea8551214a74ea2e4c8cd4954f8a28
Sridhar <sreedharriyer@gmail.com>
2017-01-27 10:29:20 -0800

    checks for airflow workers if none present start the worker nodes

---
+++ shell/airflow_worker_monitor.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+#
+# SCRIPT: airflow_worker_monitor
+# AUTHOR: siyer
+# DATE: 2017-01-26
+# REV: 0.1.T (Valid are A, B, D, T and P)
+# (For Alpha, Beta, Dev, Test and Production)
+#
+# The script will check if airflow workers are running for
+# the specific user. If not found, it will start the workers.
+
+AIRFLOW_HOME=$HOME/airflow
+AIRFLOW_VENV=$AIRFLOW_HOME/venv/bin
+
+count=`ps -ef | grep celeryd | grep $(whoami) | wc -l`
+
+if [ $count -le 1 ]
+then
+    source $AIRFLOW/activate
+    nohup airflow worker& >> $AIRFLOW_HOME/logs/worker.log
+    echo "$(date) INFO: Airflow workers started" >> /home/stagemaverick/planned-vs-actual/cron.log
+fi

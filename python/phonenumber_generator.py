c7e39a610c7caf7d1e5d2b81fbffb228e23fcc38
Sridhar <sreedharriyer@gmail.com>
2017-03-23 20:35:08 -0700

    New additions

--- python/phonenumber_generator.py
+++ python/phonenumber_generator.py
@@ -1,11 +1,13 @@
 # A ten digit phone number generator
-phonenumber = ''
-for i in range(0,10):
-    if i == 0:
-        phonenumber = str(random.randint(0,9))
-    elif i == 3 or i == 6:
-        phonenumber = phonenumber + '-' + str(random.randint(0,9))
-    else:
-        phonenumber = phonenumber + str(random.randint(0,9))
 
-print(phonenumber)
+def ph_gen():
+    phonenumber = ''
+    for i in range(0,10):
+        if i == 0:
+            phonenumber = str(random.randint(0,9))
+        elif i == 3 or i == 6:
+            phonenumber = phonenumber + '-' + str(random.randint(0,9))
+        else:
+            phonenumber = phonenumber + str(random.randint(0,9))
+    print(phonenumber)
+    return phonenumber
--- python/random_name_generator.py
+++ python/random_name_generator.py
@@ -1,19 +1,6 @@
 import random
 import os
 
-#data = open(r"C:\Users\python\names.txt").readlines()
-#print(random.choice(data))
-#print(random.randint(20,50))
-
-
-# phone number generator
-phonenumber = ''
-for i in range(0,10):
-    if i == 0:
-        phonenumber = str(random.randint(0,9))
-    elif i == 3 or i == 6:
-        phonenumber = phonenumber + '-' + str(random.randint(0,9))
-    else:
-        phonenumber = phonenumber + str(random.randint(0,9))
-
-print(phonenumber)
+data = open(r"C:\Users\Sridhar\Documents\src\python\names.txt").readlines()
+print(random.choice(data))
+print(random.randint(20,50))

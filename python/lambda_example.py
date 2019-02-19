0d4349e69037020d201fe86e392303d7819d8986
Sridhar <sreedharriyer@gmail.com>
2019-02-19 22:35:42 +0530

    lambda message

--- python/lambda_example.py
+++ python/lambda_example.py
@@ -6,3 +6,14 @@ print(x(10))
 x = lambda a, b : (a * 10) + (b * 20)
 
 print(x(10,20))
+
+
+def func(n):
+    return lambda a: a*n
+
+
+doubleValue = func(2)
+tripleValue = func(3)
+
+print(doubleValue(10))
+print(tripleValue(10))

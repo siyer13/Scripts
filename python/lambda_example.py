1b9d5e45b0e8af3c3db6f895cc2d98bdf1e0667d
Sridhar <sreedharriyer@gmail.com>
2019-02-20 12:02:14 +0530

    lambda example

--- python/lambda_example.py
+++ python/lambda_example.py
@@ -17,3 +17,27 @@ tripleValue = func(3)
 
 print(doubleValue(10))
 print(tripleValue(10))
+
+
+def fahrenheit(T):
+    return ((float(9)/5)*T + 32)
+def celsius(T):
+    return (float(5)/9)*(T-32)
+temp = (36.5, 37, 37.5,39)
+
+F = map(fahrenheit, temp)
+C = map(celsius, F)
+
+print(list(F))
+print(list(C))
+
+import string
+
+vowels = ['a', 'e', 'i', 'o', 'u']
+
+alphabets = list(string.ascii_lowercase)
+
+
+output = filter(lambda vowel : (vowel in vowels), alphabets)
+
+print(list(output))

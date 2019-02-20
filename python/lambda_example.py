372b449607abd4cd76d3bc9ee0c7e8ec58845b05
Sridhar <sreedharriyer@gmail.com>
2019-02-20 12:08:51 +0530

    lambda expressions

--- python/lambda_example.py
+++ python/lambda_example.py
@@ -41,3 +41,16 @@ alphabets = list(string.ascii_lowercase)
 output = filter(lambda vowel : (vowel in vowels), alphabets)
 
 print(list(output))
+
+countries = ['india', 'america', 'kenya', 'sweeden', 'singapore', 'peru']
+
+cap_country = list(map(lambda country : country.upper(), countries))
+
+print(cap_country)
+
+from functools import reduce
+
+number = [1,2,3,4,5]
+
+final_number = reduce(lambda num1, num2 : num1 + num2 , number)
+print(final_number)

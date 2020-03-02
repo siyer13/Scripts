edbba7af12f5d12089982be8329e74782a3328c5
Sridhar <sreedharriyer@gmail.com>
2020-03-02 20:28:05 +0530

    Month To Number

---
+++ python/month_to_num.py
@@ -0,0 +1,18 @@
+def month_to_num(short_month):
+    return {
+        'Jan': 1,
+        'Feb': 2,
+        'Mar': 3,
+        'Apr': 4,
+        'May': 5,
+        'Jun': 6,
+        'Jul': 7,
+        'Aug': 8,
+        'Sep': 9,
+        'Oct': 10,
+        'Nov': 11,
+        'Dec': 12
+    }[short_month]
+
+
+print(month_to_num('Jan'))
